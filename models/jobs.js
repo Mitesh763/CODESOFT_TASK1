ecc22ca1ec82c3d987cd619ba37f37abcf00089c
Mitesh <vmitesh9317@gmail.com>
2024-05-08 22:32:52 +0530

    changes updated

--- models/jobs.js
+++ models/jobs.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/codesoft").then(()=>{ console.log("connected") });
+// mongoose.connect("mongodb://127.0.0.1:27017/codesoft").then(()=>{ console.log("connected") });
 
 const jobSchema = mongoose.Schema({
     title: {
